dc14cb2ea7adbea0b7bca42ab6c5af348b206704
Denis Moini <denismoini09@gmail.com>
2024-06-18 04:48:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import getPosts, { getPostsLength } from "./postController.js";
+console.log(getPosts())
+console.log(getPostsLength())
+//const {generateRandomNumber, celciusToFahrenheit} = require('./utils')
+
+//console.log(`Random Number: ${generateRandomNumber()}`)
+//console.log(`32 degerees is ${celciusToFahrenheit(32)} Fahrenheit`)
---
+++ postController.js
@@ -0,0 +1,9 @@
+const posts = [
+    {id:1, title: 'Dude'},
+    {id:2, title: 'Mate'}
+]
+
+const getPosts = () => posts;
+
+export const getPostsLength = () => posts.length;
+export default getPosts
